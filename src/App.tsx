61057705978188caedeb63d2e9e0bf8490af01f3
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-06-22 13:42:08 -0300

    Change providers of index for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { BrowserRouter, Route } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 
 import { AuthContextProvider } from 'contexts/AuthContext'
 
 import { Home } from './pages/Home'
 import { NewRoom } from 'pages/NewRoom'
 
+import { GlobalStyles } from 'styles/global'
+import { theme } from 'styles/themes/default'
+
 function App() {
   return (
     <BrowserRouter>
-      <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
-      </AuthContextProvider>
+      <ThemeProvider theme={theme}>
+        <AuthContextProvider>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+        </AuthContextProvider>
+        <GlobalStyles />
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,5 @@
 import ReactDOM from 'react-dom'
-import { ThemeProvider } from 'styled-components'
 
 import App from './App'
 
-import { GlobalStyles } from 'styles/global'
-import { theme } from 'styles/themes/default'
-
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-    <GlobalStyles />
-  </ThemeProvider>,
-  document.getElementById('root'),
-)
+ReactDOM.render(<App />, document.getElementById('root'))
